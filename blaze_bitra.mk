2f5bcd5f74113123b825512e449aff1586b8f9d0
RAM-UNLOK <rakmoparte@gmail.com>
2023-12-25 13:54:53 +0530

    bitra: Add Support For ViPER4Android FX Redesign
    https://github.com/TogoFire/packages_apps_ViPER4AndroidFX

--- blaze_bitra.mk
+++ blaze_bitra.mk
@@ -24,6 +24,8 @@ $(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 # Inherit Basic Call Recorder (For Project Blaze A14)
 $(call inherit-product, vendor/bcr/bcr.mk)
 
+# ViPER4Android FX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # Project Blaze Maintainer
 BLAZE_BUILD_TYPE := OFFICIAL
@@ -35,6 +37,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_SUPPORTS_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_EXCLUDES_AUDIOFX := true
 
 # GMS Flags
 TARGET_GAPPS_ARCH := arm64
